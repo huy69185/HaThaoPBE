2f1a0d3f77aba9d56a6ac9f865f74f24583e25fa
nguyennmk <nguyennmkse170356@gmail.com>
2024-08-18 14:12:11 +0700

    ViewOrder

---
+++ ECommerceApp/Controllers/OrderController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using ECommerceApp.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace ECommerceApp.Controllers
+{
+    [Authorize]
+    public class OrderController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public OrderController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> ViewOrder()
+        {
+            // Lấy UserId của người dùng hiện tại
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            // Lấy các đơn hàng thuộc về người dùng hiện tại
+            var orders = await _context.Orders
+                .Include(o => o.OrderItems)
+                .Where(o => o.UserId == userId)
+                .ToListAsync();
+
+            // Trả về View với danh sách đơn hàng của người dùng hiện tại
+            return View(orders);
+        }
+    }
+}
